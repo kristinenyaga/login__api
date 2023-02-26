2a57d523bfbfcc220deeec02148bbcde3501f255
krisine <kristine.nyaga@student.moringaschool.com>
2023-02-26 12:41:07 +0300

    full api

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+user1=User.create(email: "kris@gmail.com", password: "pass1234")
+user2=User.create(email: "angie@gmail.com", password: "pass1234")
+user3=User.create(email: "raph@gmail.com", password: "pass1234")
+user4=User.create(email: "sam@gmail.com", password: "pass1234")
+user5=User.create(email: "esther@gmail.com", password: "pass1234")
+user6=User.create(email: "lecturer@gmail.com", password: "pass1234")
